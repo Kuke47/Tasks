f804be9afcb5c2f0e7c72dcf645e20a62f0dd428
Kuke47 <142496497+Kuke47@users.noreply.github.com>
2023-08-17 17:15:53 +0400

    Add files via upload

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,20 @@
+﻿// task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	cout << 1;
+	for (int i = m; i!= 1; i=(i+m-1)%n){
+		if (i == 0) {
+			i = n;
+		}
+		cout << i;
+	}
+	return 0;
+}
